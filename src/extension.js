397244451b0f5f4d46c28837cc7e49ded6b67212
YouWei Zhao <zhao.uv@gmail.com>
2020-11-21 20:53:39 +0800

    move out webview as html

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,6 @@
 const vscode = require("vscode");
 const path = require("path");
+const fs = require("fs");
 
 /** @param {vscode.ExtensionContext} context */
 function activate(context) {
@@ -28,10 +29,15 @@ function activate(context) {
                 // Handle messages from the webview
                 currentPanel.webview.onDidReceiveMessage(
                     message => {
+
                         switch (message.command) {
                             case 'alert':
                                 console.log(message.text);
                                 currentPanel.webview.postMessage({ command: 'refactor' });
+
+                                return;
+                            case 'editCurrentLine':
+                                console.log(message);
                                 return;
                         }
                     },
@@ -48,61 +54,16 @@ function activate(context) {
             }
         })
     );
+    
+    context.subscriptions.push(
+        vscode.commands.registerCommand('markdownDraw.editCurrentLine', () => {
+            if (currentPanel) currentPanel.webview.postMessage({ command: 'currentLine', content: String(Math.random()) });
+        })
+    );
 
 }
 exports.activate = activate;
 
 function getWebviewContent() {
-    return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Draw Note</title>
-</head>
-<body>
-    <h1 id="lines-of-code-counter">0</h1>
-
-    <script>
-        const counter = document.getElementById('lines-of-code-counter');
-
-        let count = 0;
-        setInterval(() => {
-            counter.textContent = count++;
-        }, 100);
-
-        // Handle the message inside the webview
-        window.addEventListener('message', event => {
-
-            const message = event.data; // The JSON data our extension sent
-
-            switch (message.command) {
-                case 'refactor':
-                    count = Math.ceil(count * 0.5);
-                    counter.textContent = count;
-                    break;
-            }
-        });
-    </script>
-    <script>
-        (function() {
-            const vscode = acquireVsCodeApi();
-            const counter = document.getElementById('lines-of-code-counter');
-
-            let count = 0;
-            setInterval(() => {
-                count = ~~counter.textContent;
-
-                // Alert the extension when our cat introduces a bug
-                if (Math.random() < 0.001 * count) {
-                    vscode.postMessage({
-                        command: 'alert',
-                        text: '🐛  on line ' + count
-                    })
-                }
-            }, 100);
-        }())
-    </script>
-</body>
-</html>`;
+    return fs.readFileSync(path.join(__dirname, '..', 'webview.html'), { encoding: 'utf8' })
 }
