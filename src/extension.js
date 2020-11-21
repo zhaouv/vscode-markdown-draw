14f00cd0a5b3da66159045c6e3f03a0a64ec9a1d
YouWei Zhao <zhao.uv@gmail.com>
2020-11-21 22:40:24 +0800

    refactor as functions

--- src/extension.js
+++ src/extension.js
@@ -4,80 +4,96 @@ const fs = require("fs");
 
 /** @param {vscode.ExtensionContext} context */
 function activate(context) {
+
+    // values for webview status
     /** @type {vscode.WebviewPanel | undefined} */
     let currentPanel = undefined;
+
+    // values for editting status
+    /** @type {vscode.TextEditor | undefined} */
+    let currentEditor = undefined;
+    let currentLine = 0;
+
+    function createNewPanel() {
+        vscode.commands.executeCommand('workbench.action.editorLayoutTwoRowsRight');
+        // Create and show panel
+        currentPanel = vscode.window.createWebviewPanel(
+            'drawNote',
+            'Draw Note',
+            vscode.ViewColumn.Two,
+            {
+                // Enable scripts in the webview
+                // 实际上，您的Web视图应始终使用内容安全策略禁用内联脚本
+                enableScripts: true
+            }
+        );
+
+        currentPanel.webview.html = getWebviewContent();
+        // Handle messages from the webview
+        currentPanel.webview.onDidReceiveMessage(
+            message => {
+
+                switch (message.command) {
+                    case 'alert':
+                        // console.log(message.text);
+                        currentPanel.webview.postMessage({ command: 'refactor' });
+
+                        return;
+                    case 'editCurrentLine':
+                        setEditorText(message.text);
+                        return;
+                }
+            },
+            undefined,
+            context.subscriptions
+        );
+        currentPanel.onDidDispose(
+            () => {
+                currentPanel = undefined;
+            },
+            undefined,
+            context.subscriptions
+        );
+    }
+
+    function showPanel() {
+        currentPanel.reveal();
+    }
+
+    function getEditorText() {
+        let activeTextEditor = vscode.window.activeTextEditor;
+        if (activeTextEditor) {
+            currentEditor = activeTextEditor;
+        }
+        if (!currentEditor || currentEditor.document.isClosed) {
+            vscode.window.showErrorMessage('No open text editor');
+            return; // No open text editor
+        }
+        currentLine = currentEditor.selection.active.line
+
+        let text = currentEditor.document.getText(new vscode.Range(currentLine, 0, currentLine + 1, 0))
+        return text
+    }
+
+    function setEditorText(text) {
+        console.log(text);
+    }
+
 
     context.subscriptions.push(
         vscode.commands.registerCommand('markdownDraw.startDrawSVG', () => {
             if (currentPanel) {
-                currentPanel.reveal(vscode.ViewColumn.Two);
+                showPanel()
             } else {
-                vscode.commands.executeCommand('workbench.action.editorLayoutTwoRowsRight');
-                // Create and show panel
-                currentPanel = vscode.window.createWebviewPanel(
-                    'drawNote',
-                    'Draw Note',
-                    vscode.ViewColumn.Two,
-                    {
-                        // Enable scripts in the webview
-                        // 实际上，您的Web视图应始终使用内容安全策略禁用内联脚本
-                        enableScripts: true
-                    }
-                );
-
-                currentPanel.webview.html = getWebviewContent();
-                // Handle messages from the webview
-                currentPanel.webview.onDidReceiveMessage(
-                    message => {
-
-                        switch (message.command) {
-                            case 'alert':
-                                // console.log(message.text);
-                                currentPanel.webview.postMessage({ command: 'refactor' });
-
-                                return;
-                            case 'editCurrentLine':
-                                console.log(message);
-                                return;
-                        }
-                    },
-                    undefined,
-                    context.subscriptions
-                );
-                currentPanel.onDidDispose(
-                    () => {
-                        currentPanel = undefined;
-                    },
-                    undefined,
-                    context.subscriptions
-                );
+                createNewPanel()
             }
         })
     );
 
-    /** @type {vscode.TextEditor | undefined} */
-    let editor = undefined;
-    let line = 0;
-    
     context.subscriptions.push(
         vscode.commands.registerCommand('markdownDraw.editCurrentLine', () => {
 
-            let activeTextEditor = vscode.window.activeTextEditor;
-            if (activeTextEditor) {
-                editor = activeTextEditor;
-                line = editor.selection.active.line
-            }
-            if (!editor || editor.document.isClosed) {
-                vscode.window.showErrorMessage('No open text editor');
-                return; // No open text editor
-            }
-            
-            console.log(Math.random());
-            console.log(editor)
-            console.log(editor?.selection?.active?.line)
-            console.log(editor?.selection?.active?.line===line)
-            
-            let text = editor.document.getText(new vscode.Range(line, 0, line+1, 0))
+            let text = getEditorText()
             if (currentPanel) currentPanel.webview.postMessage({ command: 'currentLine', content: text });
         })
     );
